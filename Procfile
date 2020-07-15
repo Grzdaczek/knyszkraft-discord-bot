f27322e3cc6605e1d2ce65dd115f909b3d8935d4
Grzdaczek <grzdaczek2727@gmail.com>
2020-07-16 00:15:26 +0200

    Basic setup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node web.js
+worker: node index.js
---
+++ index.js
@@ -0,0 +1,15 @@
+const dotenvOk = require('dotenv').config()
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+	if (msg.content === '!status') {
+		msg.reply('Ok!')
+	}
+})
+
+client.login(process.env.DISCORD_TOKEN)
