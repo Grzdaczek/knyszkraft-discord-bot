1ce9220f1896dcabbb89a28f1ce00e0ca507670e
Grzdaczek <grzdaczek2727@gmail.com>
2020-07-17 19:59:42 +0200

    Progress command update

--- src/commands.ts
+++ src/commands.ts
@@ -24,7 +24,7 @@ export const scoreboard = async (msg: Message): Promise<void> => {
 		for (const profile of profiles) {
 			table.push({
 				name: profile.name,
-				score: advs.get(profile.id).reduce((score, adv) => adv.type !== 'recipes' && adv.done ? score+1 : score , 0)
+				score: advs.get(profile.id).reduce((score, adv) => adv.category !== 'recipes' && adv.done ? score+1 : score , 0)
 			})
 		}
 		table.sort((a, b) => a.score < b.score ? 1 : -1)
@@ -44,20 +44,34 @@ export const scoreboard = async (msg: Message): Promise<void> => {
 
 export const progress = async (msg: Message): Promise<void> => {
 	try {
-		const username = msg.content.slice(10) || msg.author.username
-		console.log({username})
-		const profile = await mojang.getProfileByUsername(username)
+		const nickname = msg.guild && msg.guild.member(msg.author).displayName || msg.author.username
+		const player = msg.content.slice(10) || nickname
+
+		// console.log({
+		// 	nickname: msg.guild.member(msg.author).displayName, 
+		// 	username: msg.author.username,
+		// 	player: msg.content.slice(10),
+		// 	chosen: player
+		// })
+
+		const profile = await mojang.getProfileByUsername(player)
 		if(!profile) {
-			msg.channel.send('Nie ma tu takiego. Spróbuj napisać \n```\n!progress [poprawna nazwa użytkownika]\n``` lub po prostu ustaw normalny pseudonim.')
+			msg.channel.send('Nie ma tu takiego. Spróbuj napisać \n```\n!progress [poprawna nazwa użytkownika]\n``` lub po prostu ustaw pseudonim zgadny z nazwą w grze.')
 			msg.react('🤦')
 			return
 		}
-		msg.react('👌')
 		
 		const advs = await serverproject.advancements(id => id === profile.id)
-		const list = advs.get(profile.id).filter(adv => adv.type !== 'recipes' && !adv.done)
-		const messages = list.map(x => `\`\`\`js\n'${x.name}'\n${Object.keys(x.criteria).join('\n')}\n\`\`\``)
-		messages.forEach(m => msg.author.send(m))
+		const list = advs.get(profile.id).filter(adv => adv.category !== 'recipes' && !adv.done)
+		const messages = list.map(x =>
+			`.${x.name.padEnd(20, '.')}: ` +
+			`${Math.floor(x.completion*100)}% ` +
+			`╠${''.padEnd(x.completion*30, '█').padEnd(30,'═')}╣ ` +
+			('[' + x.criteriaDone.length + '/' + x.criteriaDone.length+x.criteriaLeft.length + ']').padEnd(8, '') +
+			`/*${x.criteriaLeft.join(', ')}*/\n`
+		)
+		msg.react('👌')
+		msg.author.send('```css\n' + messages.join('\n') + '```')
 	}
 	catch (err) {
 		error(msg, err)
--- src/serverproject.ts
+++ src/serverproject.ts
@@ -1,28 +1,184 @@
 import path from 'path'
 import FtpClient from 'ftp'
 
-const ftp = new FtpClient()
+export type IAdvancmentCategory = 'recipes' | 'story' | 'husbandry' | 'adventure' | 'nether' | 'end'
+export interface IAdvancment {
+	name: string,
+	fullName: string,
+	category: IAdvancmentCategory,
+	criteriaDone: string[],
+	criteriaLeft: string[],
+	completion: number
+	done: boolean
+}
+export const version = '1.16_20w14a'
+export const advancmentCategories = ['recipes', 'story', 'husbandry', 'adventure', 'nether', 'end']
+export const advancmentsCriteria = {
+	adventuring_time: [
+		'badlands',
+		'badlands_plateau',
+		'bamboo_jungle',
+		'bamboo_jungle_hills',
+		'beach',
+		'birch_forest',
+		'birch_forest_hills',
+		'cold_ocean',
+		'dark_forest',
+		'deep_cold_ocean',
+		'deep_frozen_ocean',
+		'deep_lukewarm_ocean',
+		'desert',
+		'desert_hills',
+		'forest',
+		'frozen_river',
+		'giant_tree_taiga',
+		'giant_tree_taiga_hills',
+		'jungle',
+		'jungle_edge',
+		'jungle_hills',
+		'lukewarm_ocean',
+		'mountains',
+		'mushroom_field_shore',
+		'mushroom_fields',
+		'plains',
+		'river',
+		'savanna',
+		'savanna_plateau',
+		'snowy_beach',
+		'snowy_mountains',
+		'snowy_taiga',
+		'snowy_taiga_hills',
+		'snowy_tundra',
+		'stone_shore', 
+		'swamp',
+		'taiga',
+		'taiga_hills',
+		'warm_ocean',
+		'wooded_badlands_plateau',
+		'wooded_hills',
+		'wooded_mountains'
+	],
+	balanced_diet: [
+		'apple',
+		'baked_potato',
+		'beetroot',
+		'beetroot_soup',
+		'bread',
+		'carrot',
+		'chorus_fruit',
+		'cooked_beef',
+		'cooked_chicken',
+		'cooked_cod',
+		'cooked_mutton',
+		'cooked_porkchop',
+		'cooked_rabbit',
+		'cooked_salmon',
+		'cookie',
+		'dried_kelp',
+		'enchanted_golden_apple',
+		'golden_apple',
+		'golden_carrot',
+		'honey_bottle',
+		'melon',
+		'mushroom_stew',
+		'poisonous_potato',
+		'potato',
+		'pufferfish',
+		'pumpkin_pie',
+		'rabbit_stew',
+		'raw_beef',
+		'raw_chicken',
+		'raw_cod',
+		'raw_mutton',
+		'raw_porkchop',
+		'raw_rabbit',
+		'raw_salmon',
+		'rotten_flesh',
+		'spider_eye',
+		'suspicious_stew',
+		'sweet_berries',
+		'tropical_fish'
+	],
+	bred_all_animals: [
+		'bee',
+		'cat',
+		'chicken',
+		'cow',
+		'donkey',
+		'fox',
+		'hoglin',
+		'horse',
+		'llama',
+		'mooshroom',
+		'mule',
+		'ocelot',
+		'panda',
+		'pig',
+		'rabbit',
+		'sheep',
+		'strider',
+		'turtle',
+		'wolf',
+	],
+	complete_catalogue: [
+		'tabby',
+		'tuxedo',
+		'red',
+		'siamese',
+		'british_shorthair',
+		'calico',
+		'persian',
+		'ragdoll',
+		'white',
+		'black',
+		'jellie'
+	],
+	kill_all_mobs: [
+		'blaze',
+		'cave_spider',
+		'creeper',
+		'drowned',
+		'enderman',
+		'evoker',
+		'ghast',
+		'guardian',
+		'husk',
+		'magma_cube',
+		'phantom',
+		'pillager',
+		'ravager',
+		'shulker',
+		'silverfish',
+		'skeleton',
+		'slime',
+		'spider',
+		'stray',
+		'vindicator',
+		'witch',
+		'wither_skeleton',
+		'zombie',
+		'zombie_pigman',
+		'zombie_villager',
+		'elder_guardian',
+		'enderdragon',
+		'endermite',
+		'hoglin',
+		'piglin',
+		'vexe',
+		'wither',
+		'zoglin'
+	]
+}
 
 const ftpEvent = (event: string): Promise<any> => new Promise((resolve, reject) => ftp.on(event, (e) => resolve(e)))
 const ftpList = (path: string): Promise<any> => new Promise((resolve, reject) => ftp.list(path, (err, list) => resolve(list)))
 const ftpCwd = (path: string): Promise<any> => new Promise((resolve, reject) => ftp.cwd(path, (err, currentDir) => resolve(currentDir)))
 const ftpGet = (path: string): Promise<any> => new Promise((resolve, reject) => ftp.get(path, (err, stream) => resolve(stream)))
-
-export interface Advancment {
-	name: string
-	fullName: string
-	type: AdvancmentType
-	criteria: {
-		[key: string]: string
-	}
-	done: boolean
-}
-
-type AdvancmentType = 'recipes' | 'story' | 'husbandry' | 'adventure' | 'nether' | 'end'
+const ftp = new FtpClient()
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-export const advancements = async (filter?: Function): Promise<Map<string, Advancment[]>> => {
+export const advancements = async (filter?: Function): Promise<Map<string, IAdvancment[]>> => {
 	try {
 
 		ftp.connect({
@@ -37,7 +193,7 @@ export const advancements = async (filter?: Function): Promise<Map<string, Advan
 	
 		const entryList: any[] = await ftpList('./')
 		const files = new Map()
-		const advancements: Map<string, Advancment[]> = new Map()
+		const advancements: Map<string, IAdvancment[]> = new Map()
 	
 		for (const entry of entryList) {
 			const id = path.basename(entry.name, '.json').replace(/\W/gi, '')
@@ -53,20 +209,28 @@ export const advancements = async (filter?: Function): Promise<Map<string, Advan
 	
 		ftp.end()
 	
+		//for evry player
 		for (const entry of files.entries()) {
 			const [id, file] = entry
-			const advs: Advancment[] = []
+			const advs: IAdvancment[] = []
+
+			//for evry advancment of that player
 			for (const key in file) {
 				const found = key.match(/minecraft:(\w+)/)
 				if(!found) continue
 				const adv = file[key]
-				const type: AdvancmentType = found[0].slice(10) as any
-				const name = key.slice(type.length + 11)
+				const category: IAdvancmentCategory = found[0].slice(10) as any
+				const name = key.slice(category.length + 11)
+				const criteriaDone = Object.keys(adv.criteria).map(c => c.replace(/(minecraft:)|(\w+\/)|(\.\w+)/g, ''))
+				const requiredCriteria = advancmentsCriteria[name] || []
+				const criteriaLeft = adv.done ? [] : requiredCriteria.filter(c => !criteriaDone.includes(c))
 				advs.push({
 					name,
 					fullName: key,
-					type,
-					criteria: adv.criteria,
+					category,
+					criteriaDone,
+					criteriaLeft,
+					completion: adv.done ? 1 : criteriaDone.length / (criteriaDone.length + criteriaLeft.length),
 					done: adv.done,
 				})
 			}
